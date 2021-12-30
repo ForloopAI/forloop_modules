c11a30381737db3e5f2ddce698fcb21fc66ad8ca
Dominik Vach <vach.dominik@gmail.com>
2021-12-30 01:36:13 +0100

    setup file + move to folder for packaging purposes

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+    
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name='forloop_modules',
+    version='0.1.0',
+    author='DovaX',
+    author_email='dovax.ai@gmail.com',
+    description='This package contains open source modules and integrations within Forloop.ai platform',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/ForloopAI/forloop_modules',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'doclick'
+     ],
+    python_requires='>=3.6',
+)
+    
