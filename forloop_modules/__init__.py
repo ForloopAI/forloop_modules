ce880c8525546a2cf5cbdb204de21a616fec9aa6
Dominik Vach <vach.dominik@gmail.com>
2023-09-24 20:44:58 +0200

    add errors to forloop_modules

--- forloop_modules/__init__.py
+++ forloop_modules/__init__.py
@@ -1,4 +1,4 @@
 from forloop_modules import *
 
 
-__all__=["function_handlers","globals","queries","flog","node_detail_form","utils","redis"]
+__all__=["function_handlers","globals","queries","utils","redis","errors","flog","node_detail_form"]
