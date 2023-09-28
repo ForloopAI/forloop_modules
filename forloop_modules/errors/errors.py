b2e76a0035bd24af5b5c860192ee40534a84ec06
Jakub Kawecki <jakub@forloop.ai>
2023-09-28 11:10:56 +0200

    Corrected custom exception definitions, added SoftPipelineError

--- forloop_modules/errors/errors.py
+++ forloop_modules/errors/errors.py
@@ -3,8 +3,23 @@ class CriticalPipelineError(Exception):
     Exception used for handling pipeline-breaking errors during function_handler runtime.
 
     It should be raised only in case of a critical error, when the ExecutionCore cannot recover the
-    pipeline flow, and it's execution must be terminated. Must always be raised with 'raise ... from
-    error' clause to retain the information about original errror.
+    pipeline flow, and it's execution must be terminated.
+
+    Must always be raised with 'raise ... from error' clause to retain the information about
+    original errror.
+    """
+
+
+class SoftPipelineError(Exception):
+    """
+    Exception used for handling node-breaking, but non-pipeline-breaking errors during function_handler runtime.
+
+    It should be raised only in case of a recoverable node errors, when the ExecutionCore can omit
+    execution of a given node and continue execution of a pipeline without any pipeline flow
+    disruption.
+
+    Must always be raised with 'raise ... from error' clause to retain the information about
+    original errror.
     """
 
 
@@ -25,6 +40,5 @@ class InitializationError(Exception):
     Exception raised during application/web_server start up.
 
     Raised whenever any essential module/component of the application has not been initialized
-    properly. This can be successfully setting DB/Redis connection, connecting to function_handlers
-    in a separate package, etc.
+    properly. This can mean failure while setting DB/Redis connection, initialization assertions, etc.
     """
