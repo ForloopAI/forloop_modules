ebc04ac03db5f49794afc87c2ba95e4b699b51a4
Ilya Tsakunov <ilyatsakunov@seznam.cz>
2023-10-27 12:30:56 +0200

    Handle example as list in docs

--- forloop_modules/function_handlers/auxilliary/docs.py
+++ forloop_modules/function_handlers/auxilliary/docs.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from collections import UserDict
 
+
 class Docs(UserDict):
     
     def __init__(self, description: Optional[str] = None, parameters_description: Optional[str] = None):
@@ -8,10 +9,20 @@ class Docs(UserDict):
         self.description = description
         self.parameters_description = parameters_description
         
-    def add_parameter_table_row(self, name: str, title: str, description: str, typ: Optional[str] = None, example: Optional[str] = None):
+    def add_parameter_table_row(
+            self,
+            name: str,
+            title: str,
+            description: str,
+            typ: Optional[str] = None,
+            example: Optional[str | list[str]] = None
+    ):
         if typ is None:
             typ = "Any"
-            
+
+        if isinstance(example, list):
+            example = ' | '.join(example)
+
         parameter_table_row = {
             "title": title,
             "description": description,
