2d76da481e3a9fe4468aba7e98952d4484dd15b4
Jakub Kawecki <jakub@forloop.ai>
2023-12-12 15:05:33 +0100

    BUGFIX: Added bugfixes to DataFrame handling in Results/Variables

--- forloop_modules/utils/various.py
+++ forloop_modules/utils/various.py
@@ -1,5 +1,7 @@
 import inspect
 
+import pandas as pd
+
 from forloop_modules.utils.definitions import JSON_SERIALIZABLE_TYPES, REDIS_STORED_TYPES
 
 
@@ -11,3 +13,7 @@ def is_value_redis_compatible(value) -> bool:
     return (
         type(value) in REDIS_STORED_TYPES or inspect.isfunction(value) or inspect.isclass(value)
     )
+
+
+def serialize_dataframe_variable(value_df: pd.DataFrame) -> dict:
+    return {"columns": list(value_df.columns), "values": value_df.values.tolist()}
