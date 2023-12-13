f2dc45d0f4851ca41aab151d51a77247e8c95805
Jakub Kawecki <jakub@forloop.ai>
2023-12-13 17:09:12 +0100

    NEW: Added utility functions for parsing/serializing DFs with DB and API

--- forloop_modules/utils/various.py
+++ forloop_modules/utils/various.py
@@ -1,4 +1,5 @@
 import inspect
+from typing import Any
 
 import pandas as pd
 
@@ -6,7 +7,7 @@ from forloop_modules.utils.definitions import JSON_SERIALIZABLE_TYPES, REDIS_STO
 
 
 def is_value_serializable(value) -> bool:
-    return True if type(value) in JSON_SERIALIZABLE_TYPES else False
+    return type(value) in JSON_SERIALIZABLE_TYPES
 
 
 def is_value_redis_compatible(value) -> bool:
@@ -15,5 +16,26 @@ def is_value_redis_compatible(value) -> bool:
     )
 
 
-def serialize_dataframe_variable(value_df: pd.DataFrame) -> dict:
+def serialize_dataframe_to_api(value_df: pd.DataFrame) -> dict:
+    """Serialize a DF into the dict format used by API."""
     return {"columns": list(value_df.columns), "values": value_df.values.tolist()}
+
+
+def parse_if_dataframe_from_db(row: pd.Series) -> Any:
+    """Parse a DF from a dict format used in the DB."""
+    if row['type'] == 'DataFrame':
+        df_dict = row["value"]
+        df = pd.DataFrame(df_dict["data"], index=df_dict["index"], columns=df_dict["columns"])
+        df.attrs = row["attrs"]
+        return df
+    return row['value']
+
+
+def serialize_if_dataframe_to_db(self, row: pd.Series) -> JSON_SERIALIZABLE_TYPES:
+    """Cast a DF to a dict format compatible with the DB."""
+    if row['type'] == 'DataFrame':
+        df = row["type"]
+        df_dict = df.to_dict(orient="split")
+        df_dict["attrs"] = df.attrs
+        return df_dict
+    return row['value']
