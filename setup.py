77b444d144db675e6135dfccaa19e02a9aabf261
Ilya Tsakunov <ilyatsakunov@seznam.cz>
2024-09-17 16:59:50 +0200

    Move everything to pyproject

--- setup.py
+++ setup.py
@@ -1,26 +1,28 @@
 import setuptools
-    
+
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+with open('pyproject.toml', 'r') as f:
+    project_toml = tomllib.loads(f.read())
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
-    
+
+
 setuptools.setup(
-    name='forloop_modules',
-    version='1.3.6',
-    author='DovaX',
-    author_email='dovax.ai@gmail.com',
-    description='This package contains open source modules and integrations within Forloop.ai platform',
+    name=project_toml['project']['name'],
+    version=project_toml['project']['version'],
+    author=project_toml['project']['authors'][0]['name'],
+    author_email=project_toml['project']['authors'][0]['email'],
+    description=project_toml['project']['description'],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/ForloopAI/forloop_modules',
+    url=project_toml['project']['urls']['Repository'],
     packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=[
-          ''
-     ],
-    python_requires='>=3.6',
+    classifiers=project_toml['project']['classifiers'],
+    install_requires=project_toml['project']['dependencies'],
+    python_requires=project_toml['project']['requires-python'],
 )
-    
