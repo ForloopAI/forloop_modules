b689023dcd7963a5545535ccc494693e9fd3a716
Daniel Chmúrny <daniel123chmurny@gmail.com>
2024-09-17 13:05:39 +0200

    Format parts of script_utils

--- forloop_modules/utils/script_utils.py
+++ forloop_modules/utils/script_utils.py
@@ -1,13 +1,12 @@
 import json
 import random
 import string
-
 from typing import Optional
 
 import forloop_modules.flog as flog
 import forloop_modules.queries.node_context_requests_backend as ncrb
-
 from forloop_modules.globals.active_entity_tracker import aet
+
 
 class ScriptNotFoundError(Exception):
     """Raised when no script is found for a given `project_uid`."""
@@ -18,7 +17,9 @@ class AmbiguousRequestError(Exception):
     pass
 
 def create_new_script(script_name: str = "untitled", text: str = ""):
-    """Creates a new script on the API and in case of a successful call it stores it's uid into auth --> aet.active_script_uid.
+    """
+    Creates a new script on the API and in case of a successful call it stores it's uid intoauth
+    --> aet.active_script_uid.
 
     Args:
         script_name (str, optional): A name of the new script. Defaults to "untitled".
@@ -34,14 +35,19 @@ def create_new_script(script_name: str = "untitled", text: str = ""):
         flog.error(f'Error {response.status_code}: {response.reason}.')
         
 def update_active_script(code: str, script_name: Optional[str] = "untitled"):
-    """If active script already exists, i.e. aet.active_script_uid is not None, the last active script is updated, else a new script is initialized.
+    """
+    If active script already exists, i.e. aet.active_script_uid is not None, the last active script
+    is updated, else a new script is initialized.
 
     Args:
         code (str): Text to be put into an updated or a new script as its code.
-        script_name (Optional[str], optional): A new name of an existing script (in case of creating a new one, the name won't be passed). Defaults to None.
+        script_name (Optional[str], optional): A new name of an existing script (in case of creating
+            a new one, the name won't be passed). Defaults to None.
     """    
     if aet.active_script_uid is not None:
-        response = ncrb.update_script_by_uid(aet.active_script_uid, script_name=script_name, text=code)
+        response = ncrb.update_script_by_uid(
+            aet.active_script_uid, script_name=script_name, text=code
+        )
         
         if not response.ok:
             flog.error(f'Error {response.status_code}: {response.reason}.')
@@ -55,11 +61,17 @@ def get_script_by_name(script_name: str):
         raise Exception(f'Error {response.status_code} - {response.reason}.')
     
     scripts = response.json()["result"]["scripts"]
-    scripts_matching_name = [script for script in scripts if script["script_name"] == script_name 
-                                and script["project_uid"] == aet.project_uid]
+    scripts_matching_name = [
+        script
+        for script in scripts
+        if script["script_name"] == script_name
+        and script["project_uid"] == aet.project_uid
+    ]
     
     if len(scripts_matching_name) == 0:
-        raise ScriptNotFoundError(f'No script named "{script_name}" found for project_uid == {aet.project_uid}.')
+        raise ScriptNotFoundError(
+            f'No script named "{script_name}" found for project_uid == {aet.project_uid}.'
+        )
     elif len(scripts_matching_name) > 1:
         message = f'Ambiguous request: {len(scripts_matching_name)} scripts named "{script_name}" found.'
         raise AmbiguousRequestError(message)
