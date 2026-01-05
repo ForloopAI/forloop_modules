adfbcfb6861ccd900556914ac7118fad655b6576
Dominik Vach <vach.dominik@gmail.com>
2026-01-05 16:46:26 +0100

    release 1.6.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setuptools.setup(
     name='forloop_modules',
-    version='1.6.4',
+    version='1.6.5',
     author='DovaX',
     author_email='dovax.ai@gmail.com',
     description='This package contains open source modules and integrations within Forloop.ai platform',
@@ -19,7 +19,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires=[
-          ''
+          'flogg'
      ],
     python_requires='>=3.6',
 )
